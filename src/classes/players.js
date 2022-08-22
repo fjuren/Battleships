87e3164747710c2d12b032dd4dfa99428877bffc
Fabian Juren <fjuren@gmail.com>
2022-08-22 08:04:13 -0700

    Add checkRepeatedMove method for Players class and did preliminary testing

--- src/classes/players.js
+++ src/classes/players.js
@@ -6,22 +6,52 @@ const newGame = new Gameboard(gameboard)
 class Players {
     constructor(name, preventRepeatMove) {
         this.name = name; // STRING
-        this.preventRepeatMove = preventRepeatMove; // ARRAY
+        this.preventRepeatMove = []; // ARRAY
     }
+
+    // [(1,1), (4,6), (5,5)]
 
     // number number gameboard -> gameboard
     // player takes an attacking move on the gameboard
     playerAttack = (index1, index2) => {
-        // will need to check if there was prior move at these coords
-        return newGame.receiveAttack(index1, index2)
+        if (this.checkRepeatedMove(index1, index2) == false) {
+            return false
+        };
+        this.preventRepeatMove.push([index1, index2]);
+        return newGame.receiveAttack(index1, index2);
     }
 
+    checkRepeatedMove = (index1, index2) => {
+        var counter = 0
+
+        for (let i = 0; i < this.preventRepeatMove.length; i++) {
+            if (this.preventRepeatMove[i][0] == index1 && this.preventRepeatMove[i][1] == index2) {
+                counter++
+            }
+        } 
+        console.log(counter)
+        if (counter != 0) {
+            return true // repeated move
+        } else {
+            return false // not a repeated move
+        }
+    }
 }
 
 module.exports = Players
 
-// const player1 = new Players("Fabian", [])
+const player1 = new Players("Fabian", [])
+
 
 // player1.playerAttack(3,3)
+// player1.playerAttack(5,2)
+// player1.playerAttack(1,1)
+// player1.playerAttack(9,8)
+
+
+// console.log(newGame.receiveAttack(index1, index2))
+// console.log(player1.checkRepeatedMove(3,3))
+// console.log(typeof(player1.preventRepeatMove[0]))
+// console.log(player1.preventRepeatMove[0][1])
 
 // console.log(gameboard)
--- src/test/players.test.js
+++ src/test/players.test.js
@@ -5,9 +5,23 @@ const Players = require('../classes/players')
 const newGame = new Gameboard(gameboard)
 const player1 = new Players("Fabian", [])
 
-describe("Test player moves on board", () => {
+player1.playerAttack(4,4)
+
+describe("Testing playerAttack method", () => {
     test("Check that attack move works when a ship isn't present", () => {
         expect(player1.playerAttack(3,2)).toBe('X')
+    })
+    test("With checkRepeatedMove enabled; Already attacked coordinate should be prevented from re-attacking", () => {
+        expect(player1.playerAttack(4,4)).toBe('X')
+    })
+})
+
+describe("Test whether a certain coordinate was already attacked (checkRepeatedMove tesing in isolation)", () => {
+    test("First time attacking the specified coordinate", () => {
+        expect(player1.checkRepeatedMove(5,6)).toBe(false)
+    })
+    test("Player has already attacked at the specific coordinate", () => {
+        expect(player1.checkRepeatedMove(4,4)).toBe(true)
     })
 })
 
