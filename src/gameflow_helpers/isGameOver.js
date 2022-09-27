7949762300aebf516d4245296fb396520cd18a69
Fabian Juren <fjuren@gmail.com>
2022-09-26 20:59:49 -0700

    Add basic 'play again' functionality, using page refresh onclick

--- src/gameflow_helpers/isGameOver.js
+++ src/gameflow_helpers/isGameOver.js
@@ -21,13 +21,17 @@ const isGameOver = (playerClass) => {
     btnDiv.appendChild(btn)
     btn.appendChild(btnText);
     div.insertAdjacentElement("afterend", btnDiv);
+
+    btn.setAttribute("onclick", playAgain)
+    btn.onclick = playAgain;
 }
 
 // helper for isGameOVer
 // Functionality to reset game boards after clicking 'PLay again' button
 
+
 const playAgain = () => {
-    
+    window.location.reload()
 }
 
 module.exports = isGameOver
