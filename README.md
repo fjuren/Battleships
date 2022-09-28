1f569d643ff04d97e6d78ed04cbd7086956123e7
Fabian Juren <fjuren@gmail.com>
2022-09-27 22:23:56 -0700

    Add basic media query for iPhone responsiveness

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
 # Battleships
 
-## Have fun!
+## Link to game:
 
 https://fjuren.github.io/Battleships/
 
 
 ## Purpose:
 
-Battleships is a mini project that I built as part of my self-taught journey toward becoming a web developer. The primary purpose of this project was to build an understanding of basic unit testing using Jest, but also to let me practice vanilla Javascript before diving into JS libraries. It was also to let me practice and better understand webpack and eslint.
+Battleships is a mini project that I built as part of my self-taught journey toward becoming a web developer. The primary purpose of this project was to build an understanding of basic unit testing using Jest, but also to let me practice vanilla Javascript before learning JS libraries. This project was also to help me better understand webpack and ESlint.
 
-Two things I tried to consider during this project was to reuse functionality where ever I could and to ensure each function had only one job (following programming SOLID principles).
+Two things I tried to do during this project was to 1) reuse functionality where ever I could and to 2) ensure each function had only one job to do with appropriate helper functions.
 
-Overall this project was a blast to build. An improvement I might add next is to build a better computer algorithm, making the game more challenging and fun. Building this algorithm isn't in line wiht my personal roadmap toward web development, but I intend to come back to this when I'm looking for a different challenge!
+An improvement I might add to a next iteration is to build a better computer algorithm, making the game more challenging and fun. Building this algorithm isn't in line wiht my personal roadmap toward web development, but I intend to come back to this when I'm looking for a different kind of challenge!
+
 
 ## Game Features:
 
--- src/gameflow_helpers/placeRandShips.js
+++ src/gameflow_helpers/placeRandShips.js
@@ -1,18 +1,4 @@
-// const Gameboard = require('../classes/gameboard');
-// const tempBoardSelection = require('../setup/playerOneGameboard')
-// // const tempGameboard = new Gameboard(tempBoardSelection)
 
-// const { Carrier, Battleship, Cruiser, Submarine, Destroyer, computerCarrier, computerBattleship, computerCruiser, computerSubmarine,  computerDestroyer } = require('../setup/createShips');
-// const humanShips = []
-// humanShips.push(Carrier, Battleship, Cruiser, Submarine, Destroyer)
-// const computerShips = []
-// computerShips.push(computerCarrier, computerBattleship, computerCruiser, computerSubmarine, computerDestroyer);
-
-// // temporary board for rendering purposes
-// var temp = new Gameboard(tempBoardSelection)
-
-// board ship(as an array) -> gamebaord
-// randomly selects position for ships
 const placeRandShips = (temp, arrayOfShips) => {
 var randomIndex1 =  Math.floor(Math.random() * 10);
 var randomIndex2 =  Math.floor(Math.random() * 10);
@@ -23,33 +9,8 @@ var randomIndex2 =  Math.floor(Math.random() * 10);
       randomIndex2 =  Math.floor(Math.random() * 10);
     }
   });
-  // console.log(temp)
   return temp
 }
-
-// // board ship(as an array)-> board html styling
-// // Renders of temporary ship placement  
-// const renderTempBoard = (temp, arrayOfShips) => {
-//   // first clear the board since the user can repeatedly place their starting ship positioning.
-//   temp.clearBoard();
-//   var forBoardRender = placeRandShips(temp, arrayOfShips);
-//   renderColoring(forBoardRender, '#B0E0E6')
-// }
-
-// // array color(as a string) -> board html syling
-// // applies color rendering of ship positions
-// const renderColoring = (array, color) => {
-//   array.playerBoard.forEach((row, rowInd) => {
-//     row.forEach((col, colInd) => {
-//       // Render a clear board each time this function is called
-//       document.getElementById(`p1-col${[colInd]}`).getElementsByClassName('p1-row')[`${rowInd}`].style.backgroundColor = ''
-//       // render new positions
-//       if (col != null) {
-//         document.getElementById(`p1-col${[colInd]}`).getElementsByClassName('p1-row')[`${rowInd}`].style.backgroundColor = color
-//       }
-//     })
-//   })
-// }
 
 
 module.exports = placeRandShips
